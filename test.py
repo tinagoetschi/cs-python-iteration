42f61137f0f8422374bb6c37297afd701e6aa01d
Tina Götschi <tina@ada.ac.uk>
2025-02-06 17:10:48 +0000

    Added more decision examples

--- test.py
+++ test.py
@@ -1,21 +1,44 @@
-user_age = 10
-membership_type = 'PART TIME'
-cost = 100
-if membership_type == 'FULL':
-    if user_age < 16:
-        discount = 50		
-    elif user_age > 65:
-        discount = 25
-    else:
-        discount = 0
-else:
-    if (user_age > 18) and  (user_age < 25):
-        discount = 30
-    else:
-        discount = 0
+'''my_list = []
 
-cost = cost - cost * discount / 100
-print('membership will cost you £' + str(cost))
+for num in range(5):
+    my_list += [num*num]
+
+print(my_list)
+'''
+
+'''list_of_values = []
+for i in range(0,20,2):
+    if i % 6 != 0:
+        if i % 3 == 0:
+            list_of_values += [i]
+        elif i % 2 == 0:
+            list_of_values += [i]
+print(list_of_values)
+'''
+
+
+user_age = [10,15,21,67,12,18,65]
+membership_type = ['PART TIME','FULL', 'PART TIME','FULL', 'FULL', 'PART TIME','FULL']
+cost = [100, 120, 250, 60, 300, 150, 200]
+price = []
+discount=[0,0,0,0,0,0,0]
+disc_amount=[]
+for i in range (7):
+    if membership_type[i] == 'FULL':
+        if user_age[i] < 16:
+            discount[i] = 50		
+        elif user_age[i] > 65:
+            discount[i] = 25
+        else:
+            discount[i] = 0
+    else:
+        if (user_age[i] > 18) and  (user_age[i] < 25):
+            discount[i] = 30
+        else:
+            discount[i] = 0
+    disc_amount += [cost[i] * discount[i] / 100]
+    price += [cost[i] - cost[i] * discount[i] / 100]
+    print('membership will cost you £' + str(cost[i]),'-', disc_amount[i], 'price =' ,price[i],  ' discount was=', discount[i], '% £',disc_amount[i])
 
 
 
